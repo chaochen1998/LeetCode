1ce5b85a13386da5e3025b08c2229ef882fcf07c
CC <93915716+SANJINGSHOU14@users.noreply.github.com>
2023-08-14 23:59:37 +0800

    Update AlgorithmLog.py

--- AlgorithmLog.py
+++ AlgorithmLog.py
@@ -325,3 +325,14 @@ class Solution:
                 out.pop()
         dfs(target,k)
         return res
+
+#===============================================================================
+# date: 2023/08/14
+#===============================================================================
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for i in nums:
+            res = res + [[i] + num for num in res]
+            print(res)
+        return res
