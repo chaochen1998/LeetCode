0672297200b698da0ce4359165249d749e971d60
Chao chen <chao299@outlook.com>
2024-07-29 23:26:43 +0800

    48.旋转图像

--- AlgorithmLog.py
+++ AlgorithmLog.py
@@ -9,6 +9,21 @@ coresponding code
 """
 
 #===============================================================================
+# date: 2024/07/29 48.旋转图像
+#===============================================================================
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        # Python 这里不能 matrix_new = matrix 或 matrix_new = matrix[:] 因为是引用拷贝
+        matrix_new = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                matrix_new[j][n - i - 1] = matrix[i][j]
+        # 不能写成 matrix = matrix_new
+        matrix[:] = matrix_new
+
+#===============================================================================
 # date: 2024/07/29 19.删除链表的倒数第N个节点
 #===============================================================================
 
