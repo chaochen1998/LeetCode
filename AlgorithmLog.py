5f7d21ae32837bca538555060a21f77d223d7e7d
Chao chen <chao299@outlook.com>
2024-07-30 23:08:11 +0800

    206.反转链表

--- AlgorithmLog.py
+++ AlgorithmLog.py
@@ -9,6 +9,33 @@ coresponding code
 """
 
 #===============================================================================
+# date: 2024/07/30 206.反转链表
+#===============================================================================
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        双指针方法
+        """
+        if not head or not head.next:
+            return head
+        dummy = None
+        cur = head
+        while cur:
+            tmp = cur.next
+            cur.next = dummy
+            dummy = cur
+            cur = tmp
+        return dummy
+
+
+#===============================================================================
 # date: 2024/07/30 160.相交链表
 #===============================================================================
 
