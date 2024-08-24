ee7da9250c6d14978d76b7b115116cda102c046a
Chao chen <chao299@outlook.com>
2024-08-24 10:39:46 +0800

    "236.最近的公共祖先"

--- AlgorithmLog.py
+++ AlgorithmLog.py
@@ -10,6 +10,28 @@ coresponding code
 import collections
 
 #===============================================================================
+# date: 2024/08/24 236.二叉树的最近公共祖先
+#===============================================================================
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None or root is p or root is q:
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if left and right:
+            return root
+        if left:
+            return left
+        else:
+            return right
+#===============================================================================
 # date: 2024/08/24 437.路径总和Ⅲ
 #===============================================================================
 # Definition for a binary tree node.
